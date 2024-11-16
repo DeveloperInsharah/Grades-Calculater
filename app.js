8af2a9c076a5950777d3a8f426424da911c5a444
Insharah's Developer <insharahattariya@gmail.com>
2024-11-16 04:22:24 -0800

    Done

---
+++ app.js
@@ -0,0 +1,7 @@
+let percentage = prompt("Please Enter Your Percentage To See Grades")
+if(percentage<=49){console.log("You Are Failed")}
+else if(percentage<=59){console.log("D Grade")}
+else if(percentage<=69){console.log("C Garde")}
+else if(percentage<=89){console.log("B Grade")}
+else if(percentage<=100){console.log("A Grade")}
+else{console.log("Please enter correct percentage")}
