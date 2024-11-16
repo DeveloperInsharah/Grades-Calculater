6a56a9549e9d75789254ec60798b2e45a1317e53
Insharah's Developer <insharahattariya@gmail.com>
2024-11-16 04:35:26 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let percentage = prompt("Please Enter Your Percentage To See Grades")
+if(percentage>=90 && percentage<=100){console.log("A Grade")}
+else if(percentage>=70 && percentage<=89){console.log("B Grade")}
+else if(percentage>=60 && percentage<=69){console.log("C Grade")}
+else if(percentage>=50 && percentage<=59){console.log("D Grade")}
+else{conaole.log("Please Enter Correct Percentage")}
